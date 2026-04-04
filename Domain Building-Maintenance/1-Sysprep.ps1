1b3e1aa2fd3bbe8728ff06974eb9007abc3cc236
Jonathan Edwards <76668213+SP1R1T-5@users.noreply.github.com>
2026-04-04 17:42:05 -0400

    Update and rename 1-RenameHostname.ps1 to 1-Sysprep.ps1

--- Domain Building-Maintenance/1-Sysprep.ps1
+++ Domain Building-Maintenance/1-Sysprep.ps1
@@ -1,16 +1,4 @@
 # ==============================
-# Rename Computer
-# ==============================
-$NewHostname = "DOG-DC#"
-Write-Host "`n=== Renaming Computer to '$NewHostname' ===" -ForegroundColor Cyan
-if ($env:COMPUTERNAME -ne $NewHostname) {
-    Rename-Computer -NewName $NewHostname -Force
-    Write-Host "Renamed. Reboot required." -ForegroundColor Green
-} else {
-    Write-Host "Already named '$NewHostname'. Skipping." -ForegroundColor Gray
-}
-
-# ==============================
 # Run Sysprep, we're cloning on prox
 # ==============================
 Write-Host "`n=== Running Sysprep ===" -ForegroundColor Cyan
